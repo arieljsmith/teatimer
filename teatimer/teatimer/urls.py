90f8f296a6300e0d4e6dd99e8543ad15a922a577
Ariel Smith <arieljeansmith@gmail.com>
2022-03-26 18:23:26 -0700

    Create register.html, register url, register view

--- teatimer/teatimer/urls.py
+++ teatimer/teatimer/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from core import views
 from core.views import front
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', front, name='front'),
+    path('register/', views.register_request, name="register"),
 ]
